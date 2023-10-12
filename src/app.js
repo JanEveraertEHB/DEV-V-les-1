e0c5edfa28b52b1bff57c6311518bb71c027c942
Jan Everaert <jan@crashlab.be>
2023-10-12 10:35:19 +0200

    refactor: adding file structure

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const knex = require('knex');
 const bodyParser = require('body-parser');
+const initEndpoints = require("./routes");
 
 
 // require and manage database connection
@@ -11,6 +12,11 @@ const db = knex(knexfile.development);
 // initialise app and routes;
 const app = express();
 app.use(bodyParser.json());
+
+
+
+initEndpoints(app, db);
+
 
 // export app
 module.exports = app;
---
+++ src/routes/students.js
@@ -0,0 +1,19 @@
+
+/**
+ * Struct student object and parameters
+ * 
+ * @param id (integer): Unique identifier for the student.
+ * @param name (string): Name of the student.
+ * @param age (integer): Age of the student.
+ * @param classgroup (string): Class group information for the student.
+ * @param grade (double): Grade information for the student.
+ * @param created_at (string): Timestamp of when the student record was created.
+ * @param updated_at (string): Timestamp of when the student record was last updated.
+ */
+
+
+function initEndpoints(app, db) {
+
+}
+
+module.exports = initEndpoints;
