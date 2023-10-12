1b548d0dbb53e2043520e0aa71370f530c5a4aa4
Jan Everaert <jan@crashlab.be>
2023-10-12 10:36:02 +0200

    docs: adding endpoint docs

--- src/routes/students.js
+++ src/routes/students.js
@@ -14,6 +14,16 @@
 
 function initEndpoints(app, db) {
 
+  /**
+   * GET /students
+   *
+   * This route retrieves a list of all students from the database.
+   * It returns a JSON array containing student records if successful.
+   *
+   * @param {object} req - The HTTP request object.
+   * @param {object} res - The HTTP response object.
+   * @returns {object} JSON response with either an array of student records or an error message.
+   */
   app.get('/students', async (req, res) => {
     db('students')
       .then((students) => {
@@ -25,6 +35,18 @@ function initEndpoints(app, db) {
       })
   });
 
+  /**
+   * GET /students/:id
+   *
+   * This route retrieves a specific student's information from the database based on the provided ID.
+   * It expects the student's ID as a parameter in the URL.
+   * If the student is found, it returns the student's information as JSON.
+   * If the student is not found, it returns a 404 Not Found error.
+   *
+   * @param {object} req - The HTTP request object containing the student ID as a parameter.
+   * @param {object} res - The HTTP response object.
+   * @returns {object} JSON response with either the student's information, a 404 error if not found, or a 500 error if an error occurs.
+   */
   app.get('/students/:id', async (req, res) => {
       const id = parseInt(req.params.id);
       db('students').where({ id }).first().then((student) => {
